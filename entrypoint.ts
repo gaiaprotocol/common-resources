86966b2d62424e109a9619188f7263e4841a37b5
yjgaia <yjdev88@gmail.com>
2024-10-30 11:56:40 +0900

    Add CORS support and improve file serving logic

--- entrypoint.ts
+++ entrypoint.ts
@@ -1,13 +1,21 @@
 import { exists } from "https://deno.land/std@0.223.0/fs/mod.ts";
 import { serveFile } from "https://deno.land/std@0.223.0/http/file_server.ts";
+import { corsHeaders } from "https://raw.githubusercontent.com/yjgaia/deno-module/main/api.ts";
 
 Deno.serve(async (req) => {
+  if (req.method === "OPTIONS") {
+    return new Response("OK", { headers: corsHeaders });
+  }
+
   const basePath = Deno.cwd() + "/public";
   const path = new URL(req.url).pathname;
   const filePath = basePath + path;
-  if (path !== "/" && await exists(filePath)) {
-    return await serveFile(req, filePath);
-  } else {
-    return await serveFile(req, basePath + "/index.html");
-  }
+
+  const response = path !== "/" && await exists(filePath)
+    ? await serveFile(req, filePath)
+    : await serveFile(req, basePath + "/index.html");
+
+  Object.assign(response.headers, corsHeaders);
+
+  return response;
 });
